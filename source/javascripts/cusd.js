0e77cd84e42f40995b6ecfb3321783fb9457e7e9
Eric Morris <elm232@cornell.edu>
2015-08-31 14:41:49 -0400

    Hook up video modals for homepage

--- source/javascripts/cusd.js
+++ source/javascripts/cusd.js
@@ -1,1 +1,25 @@
-// Blank
+//= require jquery/dist/jquery.min.js
+//= require foundation/js/foundation.js
+//= require foundation/js/foundation/foundation.reveal.js
+
+$(document).foundation({
+  reveal:{
+    dismiss_modal_class: 'video-modal__close-button',
+  }
+});
+
+$(document).on('opened.fndtn.reveal', '[data-reveal]', function () {
+  var videoContainer = $(this).children('.video-modal__video')[0];
+
+  videoContainer.innerHTML = '<iframe src="//player.vimeo.com/video/'
+  + videoContainer.attributes['data-video-id'].nodeValue
+  + '?autoplay=1" frameborder="0" height=' 
+  + videoContainer.offsetHeight 
+  + '" width="'
+  + videoContainer.offsetWidth
+  + '" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+});
+
+$(document).on('close.fndtn.reveal', '[data-reveal]', function () {
+  $(this).children('.video-modal__video')[0].innerHTML = '';
+});

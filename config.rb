af3649a612f110a052c0ebdd4dfbbc9bab190744
Eric Morris <elm232@cornell.edu>
2015-08-31 19:51:30 -0400

    Remove config comments

--- config.rb
+++ config.rb
@@ -32,20 +32,9 @@
 # Helpers
 ###
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
 end
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
 
 set :css_dir, 'stylesheets'
 
@@ -57,32 +46,23 @@ sprockets.append_path File.join root, 'bower_components'
 
 activate :directory_indexes
 
-
-# Build-specific configuration
 configure :build do
 
   set :relative_links, true
 
-  # For example, change the Compass output style for deployment
   activate :minify_css
 
-  # Minify Javascript on build
   activate :minify_javascript
 
-  # Enable cache buster
   activate :asset_hash do |opts|
     opts.ignore << 'favicon/*'
   end
 
-  # Use relative URLs
   activate :relative_assets
 
   activate :autoprefixer
 
   activate :imageoptim
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end
 
 case ENV['TARGET'].to_s.downcase
